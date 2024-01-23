967f4ccc31d18e0dec4ed8d67bda7f0ccce6d3ff
Hayyalu-Temesgen <hayyalutm@gmail.com>
2024-01-23 12:18:45 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """a class called Square"""
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: the length of the sides of a square.
+        """
+        self._size = size
