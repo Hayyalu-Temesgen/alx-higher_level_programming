17b72e6708bcc366a1b34b210dcd9cd01602342b
Hayyalu-Temesgen <hayyalutm@gmail.com>
2024-02-08 06:19:48 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,6 +5,12 @@ import json
 
 
 def from_json_string(my_str):
-    """A function that return the Python object representation of a JSON string."""
+    """A function that return the Python object representation of a JSON string.
+
+    Args:
+        my_str (str): The name of the file
+    Return:
+        The JSON string.
+    """
     return json.loads(my_str)
 
