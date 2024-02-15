1a163a274aafcb62ffe4a16e462c660d25daf060
Hayyalu-Temesgen <hayyalutm@gmail.com>
2024-02-15 01:37:27 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,12 +4,18 @@
 
 def write_file(filename="", text=""):
     """A function that writes a string to a text file.
-        
+
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
         The number of characters written.
     """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    try:
+        with open(filename, 'w', encoding="utf-8") as f:
+            return f.write(text)
+    except FileNotFoundError:
+        # If the file doesnt exist, create it and write the content
+        with open(filename, 'w', encoding='utf-8') as new_f:
+            num_chars_written = new_f.write(text)
+            return num_chars_written
